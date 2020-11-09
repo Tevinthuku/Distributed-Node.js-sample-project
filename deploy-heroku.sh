5493ed2610a975d11070a268134fbe5896e63cbb
Tevinthuku <tevinthuku@gmail.com>
2020-11-09 18:19:04 +0300

    heroku deployment

---
+++ deploy-heroku.sh
@@ -0,0 +1,5 @@
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku plugins:install @heroku-cli/plugin-container-registry
+heroku container:login
+heroku container:push web --app tev-distnode
+heroku container:release web --app tev-distnode
